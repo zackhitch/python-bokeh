5047a81abd3eb640a6eef249864df0890f57cb6f
zackhitch <zackhitch@users.noreply.github.com>
2018-06-20 16:01:23 -0700

    drawing edges properly from one vert to the next - ZH

--- src/draw.py
+++ src/draw.py
@@ -11,12 +11,12 @@ graph_data = Graph()
 graph_data.debug_create_test_data()
 print(graph_data.vertexes)
 
-N = 10
+N = len(graph_data.vertexes)
 node_indices = list(range(N))
 
-debug_pallete = Spectral8
-debug_pallete.append('#ff0000')
-debug_pallete.append('#0000ff')
+color_list = []
+for vertex in graph_data.vertexes:
+    color_list.append(vertex.color)
 
 plot = figure(title='Graph Layout Demonstration', x_range=(0, 500), y_range=(0, 500),
               tools='', toolbar_location=None)
@@ -24,17 +24,33 @@ plot = figure(title='Graph Layout Demonstration', x_range=(0, 500), y_range=(0, 
 graph = GraphRenderer()
 
 graph.node_renderer.data_source.add(node_indices, 'index')
-graph.node_renderer.data_source.add(debug_pallete, 'color')
-graph.node_renderer.glyph = Oval(height=10, width=10, fill_color='color')
+graph.node_renderer.data_source.add(color_list, 'color')
+graph.node_renderer.glyph = Oval(height=30, width=30, fill_color='color')
 
+start_points, end_points = [], []
+for vert in graph_data.vertexes:
+    for edge in vert.edges:
+        start_points.append(graph_data.vertexes.index(vert))
+        end_points.append(graph_data.vertexes.index(edge.destination))
+
+# this is drawing the edges from start to end
+#
 graph.edge_renderer.data_source.data = dict(
-    start=[0]*N,
-    end=node_indices)
+    # start=[0]*N,  # this is a list of some kind that has to do with starting points
+    start=start_points,
+    end=end_points)  # this is a lst of some kind that has to do with ending points
+print(graph.edge_renderer.data_source.data)
 
-# start of layout code
-circ = [i*2*math.pi/N for i in node_indices]
-x = [math.cos(i) for i in circ]
-y = [math.sin(i) for i in circ]
+
+# start of layout cde
+# circ = [i*2*math.pi/N for i in node_indices]
+# Looks liek this is setting the positions of the vertexes
+# x = [math.cos(i) for i in circ]
+x = [v.pos['x'] for v in graph_data.vertexes]
+# y = [math.sin(i) for i in circ]
+y = [v.pos['y'] for v in graph_data.vertexes]
+
+print(y)
 
 graph_layout = dict(zip(node_indices, zip(x, y)))
 graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
--- src/graph.py
+++ src/graph.py
@@ -19,8 +19,21 @@ class Graph:
     def debug_create_test_data(self):
         debug_vertex_1 = Vertex('t1', x=40, y=40)
         debug_vertex_2 = Vertex('t2', x=140, y=140)
+        debug_vertex_3 = Vertex('t3', x=300, y=400)
+        debug_vertex_4 = Vertex('t4', x=400, y=450)
+        debug_vertex_5 = Vertex('t5', x=340, y=350)
 
         debug_edge_1 = Edge(debug_vertex_2)
         debug_vertex_1.edges.append(debug_edge_1)
 
-        self.vertexes.extend([debug_vertex_1, debug_vertex_2])
+        debug_edge_2 = Edge(debug_vertex_3)
+        debug_vertex_2.edges.append(debug_edge_2)
+
+        debug_edge_3 = Edge(debug_vertex_4)
+        debug_vertex_3.edges.append(debug_edge_3)
+
+        debug_edge_4 = Edge(debug_vertex_5)
+        debug_vertex_4.edges.append(debug_edge_4)
+
+        self.vertexes.extend(
+            [debug_vertex_1, debug_vertex_2, debug_vertex_3, debug_vertex_4, debug_vertex_5])
