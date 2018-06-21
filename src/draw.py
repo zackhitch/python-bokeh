f4348bed365eaf93c4260b7879af00c0df506d65
zackhitch <zackhitch@users.noreply.github.com>
2018-06-21 15:53:17 -0700

    added randomize function - ZH

--- src/draw.py
+++ src/draw.py
@@ -2,14 +2,20 @@ import math
 
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval
+from bokeh.models import GraphRenderer, StaticLayoutProvider, Circle, ColumnDataSource, Range1d, LabelSet, Label
 from bokeh.palettes import Spectral8
 
 from graph import *
 
+WIDTH = 500
+HEIGHT = 500  # TODO: currently graph renders to scale, scaled to numbers here. https://stackoverflow.com/questions/21980662/how-to-change-size-of-bokeh-figure
+CIRCLE_SIZE = 30
+
 graph_data = Graph()
-graph_data.debug_create_test_data()
-print(graph_data.vertexes)
+# graph_data.debug_create_test_data()
+graph_data.randomize(5, 4, 150, 0.6)
+# print(graph_data.vertexes)
+graph_data.bfs(graph_data.vertexes[0])
 
 N = len(graph_data.vertexes)
 node_indices = list(range(N))
@@ -18,20 +24,16 @@ color_list = []
 for vertex in graph_data.vertexes:
     color_list.append(vertex.color)
 
-plot = figure(title='Graph Layout Demonstration', x_range=(0, 500), y_range=(0, 500),
+plot = figure(title='Graph Layout Demonstration', x_range=(0, WIDTH), y_range=(0, HEIGHT),
               tools='', toolbar_location=None)
 
 graph = GraphRenderer()
 
 graph.node_renderer.data_source.add(node_indices, 'index')
 graph.node_renderer.data_source.add(color_list, 'color')
-graph.node_renderer.glyph = Oval(height=30, width=30, fill_color='color')
+graph.node_renderer.glyph = Circle(size=CIRCLE_SIZE, fill_color='color')
 
 start_points, end_points = [], []
-# for vert in graph_data.vertexes:
-#     for edge in vert.edges:
-#         start_points.append(graph_data.vertexes.index(vert))
-#         end_points.append(graph_data.vertexes.index(edge.destination))
 
 for start_point, vert in enumerate(graph_data.vertexes):
     for edge in vert.edges:
@@ -41,18 +43,15 @@ for start_point, vert in enumerate(graph_data.vertexes):
 # this is drawing the edges from start to end
 #
 graph.edge_renderer.data_source.data = dict(
-    # start=[0]*N,  # this is a list of some kind that has to do with starting points
+    # this is a list of some kind that has to do with starting points
     start=start_points,
     end=end_points)  # this is a lst of some kind that has to do with ending points
 print(graph.edge_renderer.data_source.data)
 
 
 # start of layout cde
-# circ = [i*2*math.pi/N for i in node_indices]
-# Looks liek this is setting the positions of the vertexes
-# x = [math.cos(i) for i in circ]
+# Looks like this is setting the positions of the vertexes
 x = [v.pos['x'] for v in graph_data.vertexes]
-# y = [math.sin(i) for i in circ]
 y = [v.pos['y'] for v in graph_data.vertexes]
 
 print(y)
@@ -61,6 +60,26 @@ graph_layout = dict(zip(node_indices, zip(x, y)))
 graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
 plot.renderers.append(graph)
+
+
+# Create a new dictionary to use as a data source, with three lists in it, ordered in the same
+# way as vertexes
+# List of x values
+# List of y values
+# List of labels
+
+value = [v.value for v in graph_data.vertexes]  # TODO: possible optimization
+
+label_source = ColumnDataSource(data=dict(x=x, y=y, v=value))
+
+
+labels = LabelSet(x='x', y='y', text='v', level='overlay',
+                  source=label_source, render_mode='canvas', text_align='center', text_baseline='middle')
+
+
+# TODO: Investigate plot.add_layout vs. plot.renderers.append
+plot.add_layout(labels)
+
 
 output_file('graph.html')
 show(plot)
--- src/graph.py
+++ src/graph.py
@@ -1,3 +1,5 @@
+from random import random, randint
+
 
 class Edge:
     def __init__(self, destination):
@@ -37,3 +39,79 @@ class Graph:
 
         self.vertexes.extend(
             [debug_vertex_1, debug_vertex_2, debug_vertex_3, debug_vertex_4, debug_vertex_5])
+
+    def bfs(self, start):
+        print('called bfs')
+        random_color = "rgb({0},{1},{2})".format(randint(
+            0, 255), randint(0, 255), randint(0, 255))
+
+        queue = []
+        found = []
+
+        queue.append(start)
+        found.append(start)
+
+        start.color = random_color
+
+        print('about to start while')
+        while len(queue) > 0:
+            v = queue[0]
+            for edge in v.edges:
+                if edge.destination not in found:
+                    found.append(edge.destination)
+                    queue.append(edge.destination)
+                    edge.destination.color = random_color
+            queue.pop(0)  # TODO: Look at collections.dequeue
+
+        # print('about to return')
+        # return found
+
+    # create a random graph
+    def randomize(self, width, height, pxBox, probability=0.6):
+        # helper function to set up two way edges
+        def connectVerts(v0, v1):
+            v0.edges.append(Edge(v1))
+            v1.edges.append(Edge(v0))
+
+        count = 0
+
+        # build a grid of verts
+        grid = []
+        for i in range(height):
+            row = []
+            for j in range(width):
+                count += 1
+                valString = "v%s" % (count)
+                v = Vertex(valString, x=0, y=0)
+                row.append(v)
+            grid.append(row)
+
+        # go through the grid randomly hooking up edges
+        for y in range(height):
+            for x in range(width):
+                # connect down
+                if y < height - 1:
+                    if random() < probability:
+                        connectVerts(grid[y][x], grid[y + 1][x])
+
+                # connect right
+                if x < width - 1:
+                    if random() < probability:
+                        connectVerts(grid[y][x], grid[y][x + 1])
+
+        # last pass, set the x and y coordinates for drawing
+        boxBuffer = 0.8
+        boxInner = pxBox * boxBuffer
+        boxInnerOffset = (pxBox - boxInner) / 2
+
+        for y in range(height):
+            for x in range(width):
+                grid[y][x].pos['x'] = x * pxBox + \
+                    boxInnerOffset + random() * boxInner
+                grid[y][x].pos['y'] = y * pxBox + \
+                    boxInnerOffset + random() * boxInner
+
+        # finally, add everything in our grid to the vertexes in this Graph
+        for y in range(height):
+            for x in range(width):
+                self.vertexes.append(grid[y][x])
